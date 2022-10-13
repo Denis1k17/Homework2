ff4f1490078ae94734f70c892dce848c9f26c614
Denis <114667123+Denis1k17@users.noreply.github.com>
2022-10-13 23:43:15 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,14 +1,4 @@
-viewing_years = int(input("годы в течении которых посетитель будет постоянно смотреть на экспонаты"))
-print("Колличество просмотренных экспонатов:", viewing_years * 365 * 8 * 60 // 5)
-
-exhibits = int(input("Колличество экспонатов")
-
-all_time = exhibits * 5
-
-days = all_time // 480
-all_time = days * 480
-
-years = days // 365
-days %= 365
-
-print("years", years, "days", days, "mins", all_time)
+years = int(input("Сколько лет смотрим на экспонаты? "))
+print("Будет просмотрено " + str(years*365*96) + " экспонатов")
+number_exp = int(input("Сколько экспонатов нужно осмотреть? "))
+print("На просмотр уйдет " + str(number_exp//96//365) + " лет, " + str(number_exp//96) + " дней, " + str(number_exp*5) + " минут.")
